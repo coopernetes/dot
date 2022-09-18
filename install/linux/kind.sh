1c6b3ccfbbd277ec0b0009dc28ab6f5db3703644
Thomas Cooper <thomascooperca@gmail.com>
2022-09-18 03:21:22 -0400

    rename installers, kind+minikube

---
+++ install/linux/kind.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.15.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
---
+++ install/linux/minikube.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm -f minikube-linux-amd64 
+

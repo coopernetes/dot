a677bab809c773ab4d236844aaf262392a727e83
Thomas Cooper <thomascooperca@gmail.com>
2022-09-20 10:53:53 -0400

    echo downloading

--- install/linux/kind.sh
+++ install/linux/kind.sh
@@ -3,7 +3,9 @@ set -eu
 set -o pipefail
 
 VERSION=${VERSION:-0.15.0}
-curl -fsSLo ./kind https://kind.sigs.k8s.io/dl/v${VERSION}/kind-linux-amd64
+_dl="https://kind.sigs.k8s.io/dl/v${VERSION}/kind-linux-amd64"
+echo "Downloading ${_dl}..."
+curl -fsSLo ./kind ${_dl}
 chmod +x ./kind
 mv ./kind $HOME/.local/bin/kind
 
--- install/linux/minikube.sh
+++ install/linux/minikube.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+_dl="https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+echo "Downloading ${_dl}..."
+curl -fsSLO ${_dl}
 install minikube-linux-amd64 $HOME/.local/bin/minikube
 rm -f minikube-linux-amd64 
 
